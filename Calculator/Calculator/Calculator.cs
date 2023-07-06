4c746269a5370ed0f45c251f10cd2de4f124a6f6
Kateryna Danylchenko <kdanilchenko265@gmail.com>
2023-07-06 09:52:19 +0300

    Finish Calculator

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculator;
+
+public static class Calculator
+{
+
+    public static void Add(int a, int b) => Console.WriteLine(a + b);
+
+    public static void Subtract(int a, int b) => Console.WriteLine(a - b);
+
+    public static void Multiply(int a, int b) => Console.WriteLine(a * b);
+
+    public static void Divide(int a, int b)
+    {
+        try
+        {
+            Console.WriteLine(a / b);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Try again !");
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Calculator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Calculator!");
+        while (true)
+        {
+            Console.WriteLine("Choose operation:");
+            Console.WriteLine("1 - Add");
+            Console.WriteLine("2 - Subtract");
+            Console.WriteLine("3 - Multiply");
+            Console.WriteLine("4 - Divide");
+            Console.WriteLine("5 - Exit\n");
+
+
+            string input = Console.ReadLine();
+
+            if (input == "5")
+                break;
+
+            if (input != "1" && input != "2" && input != "3" && input != "4")
+            {
+                Console.WriteLine("Incorrect operation");
+                continue;
+            }
+
+            Console.Write("Enter first number: ");
+            int number1;
+            if (!int.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.WriteLine("Invalid number format!");
+                continue;
+            }
+
+            Console.Write("Enter second number: ");
+            int number2;
+            if (!int.TryParse(Console.ReadLine(), out number2))
+            {
+                Console.WriteLine("Invalid number format!\n");
+                continue;
+            }
+
+            switch (input)
+            {
+                case "1":
+                    Calculator.Add(number1, number2);
+                    break;
+                case "2":
+                    Calculator.Subtract(number1, number2);
+                    break;
+                case "3":
+                    Calculator.Multiply(number1, number2);
+                    break;
+                case "4":
+                    Calculator.Divide(number1, number2);
+                    break;
+            }
+
+            Console.WriteLine("-------------------------------------");
+        }
+    }
+}
